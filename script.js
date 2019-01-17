1efbd5d775974586dcd59c75ebc50aa21b01b9ee
avil13 <13.avil@gmail.com>
2019-01-17 03:56:12 +0300

    Updates

---
+++ script.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+
+    var Spy = $('.menu').anchorSpy({
+            margin: 100,
+            speed: 1000,
+            next: '.next',
+            prev: '.prev',
+            active_class: 'active',
+            active_parent: true
+        });
+
+
+});
